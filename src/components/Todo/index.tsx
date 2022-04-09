4bae9dd2b3ed658b79e1a32970a3c99b63812b1d
Alejandro M. Coca <alxmcr@outlook.com>
2022-04-09 00:03:02 -0400

    remove

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { ITodo } from "../../@types/appTypes";
 import { TodoListContext } from "../../providers/TodoListProvider";
+import { removeTodoAction } from "../../store/actions";
 
 export interface ITodoProps {
   todo: ITodo;
@@ -9,10 +10,14 @@ export interface ITodoProps {
 export default function Todo({ todo }: ITodoProps) {
   const { dispatch } = useContext(TodoListContext);
 
+  const onRemove = (todoToRemove: ITodo) => {
+    dispatch(removeTodoAction(todoToRemove));
+  };
+
   return (
     <li id={todo?.id}>
       {todo?.title}
-      <button>Remove</button>
+      <button onClick={() => onRemove(todo)}>Remove</button>
     </li>
   );
 }
