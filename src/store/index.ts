195ba5989dcce6561f62b8bdf4347ad6e7072fc9
Alejandro M. Coca <alxmcr@outlook.com>
2022-04-09 01:09:12 -0400

    clean up

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
-import { IRootState, rootReducer } from "./reducers";
-import { createStore, Store } from "redux";
+import { rootReducer } from "./reducers";
+import { createStore } from "redux";
 import { composeWithDevTools } from '@redux-devtools/extension';
 
 export const store = createStore(rootReducer, composeWithDevTools())
